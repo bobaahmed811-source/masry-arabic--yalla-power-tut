f431d5ce1b6ddc0b601cc60cb5d105a50bdf5ef3
boba Ahmed <bobaahmed811@gmail.com>
2025-12-12 14:15:26 +0000

    اوك

--- src/app/pronunciation-challenge/page.tsx
+++ src/app/pronunciation-challenge/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import React, { useState, useEffect, useCallback, useRef } from 'react';
@@ -353,3 +354,5 @@ export default function PronunciationChallengePage() {
     </div>
   );
 }
+
+    
