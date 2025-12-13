1fe2cb4abea8c51e8cf073c2f65ce285ad8b81a8
boba Ahmed <bobaahmed811@gmail.com>
2025-12-13 14:44:09 +0000

    لا انا لسه محطتش اى ملفات فى المستودع

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1 style={{fontSize: '5rem', fontWeight: 'bold'}}>
+          مملكة يلا مصري
+        </h1>
+        <p style={{fontSize: '2rem'}}>
+          تم إعادة التأسيس بنجاح. البوابة الآن جاهزة للعمل.
+        </p>
+      </div>
+    </main>
+  );
+}
